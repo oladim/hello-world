a246035fe0430c97f73a624d4329046a7acd7be8
Ogunrinu Abideen <ogunrinuabideen@gmail.com>
2020-03-05 13:10:42 +0100

    creating dockerfile

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var app = express();
 
-const port = 3000;
+const port = 8080;
 
 
 app.get("/", (req, res) => res.send("Hello World"));
+
+app.get("/contact", (req, res) => res.send("This is contact page"));
 
 app.listen(port, () => console.log("Server has started"));
